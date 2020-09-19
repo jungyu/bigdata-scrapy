607fa229ef54cdb950e980ae7c022ecf902edf08
Aaron Yu <jungyuyu@gmail.com>
2020-09-19 17:13:29 +0800

    Update 1-0-tutorials.py

--- 1-0-tutorials.py
+++ 1-0-tutorials.py
@@ -5,3 +5,13 @@ with open('example.txt', 'w') as f:
   f.write('some content')
 
 files.download('example.txt')
+
+
+#在 Colab 沒法寫入此檔
+import requests
+url = 'https://www.python.org/static/img/python-logo.png'
+r = requests.get(url)
+f = open('python.jpg', 'wb')
+# r.content 取得回應內容( byte stream 位元組流)
+f.write(r.content)
+f.close()
