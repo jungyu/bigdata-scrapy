8c45f662a51c3bfc3486c64bc76345539f46d0cc
Aaron Yu <jungyuyu@gmail.com>
2020-10-17 09:56:04 +0800

    Update 3-0.py

--- 3-0.py
+++ 3-0.py
@@ -3,3 +3,13 @@
 
 import requests
 from bs4 import BeautifulSoup
+
+headers_Get = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+    }
