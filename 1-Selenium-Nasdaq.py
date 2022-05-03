e1f08fd67363b32454db2d57c948da0ad35bf028
Aaron Yu <aaron@meto.com.tw>
2022-05-03 11:42:35 +0800

    加入 CC4.0 版權說明

--- 1-Selenium-Nasdaq.py
+++ 1-Selenium-Nasdaq.py
@@ -1,29 +1,18 @@
 """
-Windows 環境:
-
-
-MacOSX 環境:
-  安裝 macOS 缺少套件的管理工具 brew: https://brew.sh/index_zh-tw
-  $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-  安裝 Chrome-driver
-  $ brew install cask chromedriver
-
-  安裝 python3 (新版本預設已內含 pip)
-  $ brew install python3
-
-  安裝 pip 虛擬環境
-  $ python3 -m venv myenv
-
-  切換到指定的 pip 虛擬環境，此處為 myenv
-  $ source ~/myenv/bin/activate
-
-  安裝 selenium
-  $ python3 -m pip install selenium
-
-  安裝lxml
-  $ python3 -m pip install lxml
+說明：此程式為爬取 Nasdaq 即時價格，因此需在其營運時間內爬取，部份的時段該網站並未提供即時交易行情
+作者：Jung-Yu Yu jungyuyu@gmail.com 2022-05-02
+授權：本著作係採用創用 CC 姓名標示 4.0 國際 授權條款授權
 """
+__author__ = "Jung-Yu Yu"
+__copyright__ = "Copyright 2022, The Big Data Scrapy for Python Project"
+__credits__ = ["Jung-Yu Yu"]
+__license__ = "Creative Common"
+__version__ = "4.0"
+__maintainer__ = "Jung-Yu Yu"
+__email__ = "jungyuyu@gmail.com"
+__github__ = "https://github.com/jungyu/bigdata-scrapy"
+__status__ = "Beta"
+
 
 from lxml import etree
 
@@ -33,18 +22,26 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
+
 # 找 user-agent 的網站： https://www.whatsmyua.info/
 userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
+
 # 許多網站會檢查 user-agent
 options.add_argument("user-agent={}".format(userAgent))
-# 不讓瀏覽器執行在前台，而是在背景執行，開發前期(或在本機執行)先不加才看的到畫面
+
+# 不讓瀏覽器執行在前台，而是在背景執行。
+# 開發前期(或在本機執行)，將以下參數註解 # 不使用，才能看的到畫面
 # options.add_argument('--headless')
+
 # 在非沙盒測試環境下，可以 root 權限運行
 options.add_argument('--no-sandbox')
+
 # 不以 /dev/shm 作為暫存區(使用 /tmp)
 options.add_argument('--disable-dev-shm-usage')
+
 # 設定瀏覽器的解析度
 # options.add_argument('--window-size=1920,1080')
+
 # 關閉 GPU ，Google 文件提到需要加上這個參數以解決部份的 bug
 # options.add_argument('--disable-gpu')
 
@@ -79,8 +76,10 @@ wd.implicitly_wait(5)
 
 results = wd.find_element(By.XPATH, '//table[@class="latest-real-time-trades__table"]').get_attribute('innerHTML')
 dom = etree.HTML(results)
-dayTrades = composeTrade(dom)
 
+#TODO:交易日
+
+dayTrades = composeTrade(dom)
 #持續點擊下一頁
 nextButton = wd.find_element(By.XPATH, '//button[@class="pagination__next"]')
 while nextButton.get_attribute('disabled') is None:
