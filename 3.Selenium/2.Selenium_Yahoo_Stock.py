2e2e028675061b09d648f548ce24a114d1783b4e
Aaron Yu <jungyuyu@gmail.com>
2023-04-28 16:03:10 +0800

    修正被動畫或其它元素擋住的問題
    selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted: Element

--- 3.Selenium/2.Selenium_Yahoo_Stock.py
+++ 3.Selenium/2.Selenium_Yahoo_Stock.py
@@ -14,8 +14,9 @@ __status__ = "Beta"
 from lxml import etree
 
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import StaleElementReferenceException
@@ -38,27 +39,36 @@ def main():
     #隱式等待：是在嘗試發現某個元素的時候，如果沒能立刻發現，就等待固定長度的時間。預設設定是0秒。
     wd.implicitly_wait(5)
 
+    # 禁用動畫
+    wd.execute_script("document.body.style.webkitAnimationPlayState='paused'")
+
     #交易日
-    #//div[@id="main-0-QuoteHeader-Proxy"]/div/div[2]/div/span
     tradeDate = wd.find_element(By.XPATH, '//div[contains(@class, "qsp-ts-price-by-volumes")]/div/span').text
     print(tradeDate)
 
+    nextButtonXpath = '//span[contains(text(),"載入更多")]/parent::*/parent::button'
     #持續點擊「載入更多」
-    nextButton = wd.find_element(By.XPATH, '//span[contains(text(),"載入更多")]/parent::*/parent::button')
-    while nextButton:
-        #等待能定位到新的「載入更多」按鈕
-        bottom = WebDriverWait(wd, 10).until(expected_conditions.presence_of_element_located((By.XPATH, '//span[contains(text(),"載入更多")]/parent::*/parent::button')))
+    while wd.find_element(By.XPATH, nextButtonXpath):
+        # 等待元素可被點擊
+        wait = WebDriverWait(wd, 10)
+        element = wait.until(EC.element_to_be_clickable((By.XPATH, nextButtonXpath)))
+
+        # 移動到該元素上方再進行點擊
+        actions = ActionChains(wd)
+        actions.move_to_element(element).click().perform()
+
         #將捲軸捲到 bottom 的定位點
-        wd.execute_script("arguments[0].scrollIntoView()", bottom)
+        wd.execute_script("arguments[0].scrollIntoView()", element)
+
         #要視實際載入內容的速度，進行秒數的調整
-        time.sleep(1)
+        time.sleep(5)
         results = wd.find_element(By.XPATH, '//section[@id="qsp-ts-price-by-time"]').get_attribute('innerHTML')
         dom = etree.HTML(results)
         composeTrade(dom)
 
         try:
-            nextButton.click()
-            nextButton = wd.find_element(By.XPATH, '//span[contains(text(),"載入更多")]/parent::*/parent::button')
+            # 使用 JavaScript 點擊該元素
+            wd.execute_script("arguments[0].click();", element)
             print('.',end = '')
         except TimeoutException:
             break
